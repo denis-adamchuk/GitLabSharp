6fb194fd27c8bcc3ce6cb0d84ed64961f070b5cb
denis_adamchuk <denis.adamchuk@gmail.com>
2020-06-20 01:14:38 +0300

    Add constructor to DiscussionNote

--- src/entities/Discussion.cs
+++ src/entities/Discussion.cs
@@ -94,6 +94,21 @@ namespace GitLabSharp.Entities
    /// </summary>
    public class DiscussionNote
    {
+      public DiscussionNote(int id, string body, DateTime created_At, DateTime updated_At, User author, string type,
+         bool system, bool resolvable, bool resolved, Position position)
+      {
+         Id = id;
+         Body = body;
+         Created_At = created_At;
+         Updated_At = updated_At;
+         Author = author;
+         Type = type;
+         System = system;
+         Resolvable = resolvable;
+         Resolved = resolved;
+         Position = position;
+      }
+
       [JsonProperty]
       public int Id { get; protected set; }
 
